8e5b11439fd85eb09c01e1654e96b92fd95bb071
BMenez <bmenez@laposte.net>
2025-12-24 09:07:43 +0100

    test page admin

---
+++ src/app/sfy1024/page.js
@@ -0,0 +1,8 @@
+export default function sfy1024() {
+  return (
+    <main className="flex flex-col items-start justify-start min-h-screen text-left text-white bg-[#5f3dc4] px-6">
+      <br/>
+      <p>test</p>
+    </main>
+  );
+}
